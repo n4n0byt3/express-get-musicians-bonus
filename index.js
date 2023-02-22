82790cc3334f28b96e7031d0fa6e18a4858c95f4
n4n0byt3 <shivbo1@hotmail.com>
2023-02-22 15:01:51 +0000

    completed workshop bonus tasks

--- index.js
+++ index.js
@@ -5,6 +5,21 @@ const {Musician} = require('./Musician')
 Musician.belongsTo(Band, {foreignKey: 'band_id'})
 Band.hasMany(Musician, {foreignKey: 'band_id'})
 
+app.get('/bands', async (req, res) => {
+    const bands = await Band.findAll({
+        include: [Musician]
+    });
+    res.json(bands);
+});
+
+app.get('/bands/:id', async (req, res) => {
+    const band = await Band.findByPk(req.params.id, {
+        include: [Musician]
+    });
+    res.json(band);
+});
+
+
 module.exports = {
     Band,
     Musician
